79912bee32096f77765e8740c4605c99ff59ce01
Terry Weymouth <weymouth@umich.edu>
2022-02-14 10:48:02 -0500

    Adding Yaw components. Some problem - does not compile.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+mod pages;
+
+use pages::Home;
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<Home>::new().mount_to_body();
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
